6609008b3f58f03a1f7aa3ae959ff284121016e2
Martin Rodriguez <martin.rodriguez@upa.edu.py>
2023-11-24 11:30:21 -0300

    Primer Commit

---
+++ embaapo/WEB-INF/src/prueba/Test.java
@@ -0,0 +1,18 @@
+package prueba;
+
+public class Test {
+    
+    String valor = "Carlitos";
+public static void main (String[] args) {
+    
+}
+
+public String getValor() {
+    return valor;
+}
+
+public void setValor(String valor) {
+    this.valor = valor;
+}
+
+}
